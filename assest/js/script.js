ff42c3e0b693e5c29d383b1dfc4b4415d0dabbcd
Sofiqul Islam <37002210+SChoolWab@users.noreply.github.com>
2020-08-25 00:28:21 +0600

    Add files via upload

---
+++ assest/js/script.js
@@ -0,0 +1,9 @@
+var mixer = mixitup('.mixuer');
+
+$(document).ready(function(){
+    // Navbar
+    // $('.menu-items').hide();
+    $('#togglerIcon').click(function(){
+        $('.menu-items').toggle();
+    })
+})
